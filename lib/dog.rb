a87343c7101327ba1cd9e34944232694edadc9ea
Nii <bnstephens001@gmail.com>
2022-08-28 17:23:30 +0000

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
+   #Setter Method
+    def name=(name)
+        @name = name
+    end
 
+    #Getter Method
+        def name 
+            @name
+        end
 end
+
+class Dog
+    #Setter Method
+     def breed=(breed)
+         @breed = breed
+     end
+ 
+     #Getter Method
+         def breed 
+             @breed
+         end
+ end
